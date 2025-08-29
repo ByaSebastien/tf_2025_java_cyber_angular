beb90acf9427684036075fc8bb1a9f49e06b4580
Bya Sebastien <byaebastien@hotmail.com>
2025-08-29 14:24:58 +0200

    Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'demo1',
+    pathMatch: 'full',
+  },
+  {
+    path: 'demo1',
+    loadComponent: () => import('./demos/components/demo1/demo1').then(m => m.Demo1),
+  },
+  {
+    path: 'demo2',
+    loadComponent: () => import('./demos/components/demo2/demo2').then(m => m.Demo2),
+  },
+  {
+    path: 'demo3',
+    loadComponent: () => import('./demos/components/demo3/demo3').then(m => m.Demo3),
+  },
+  {
+    path: 'exo1',
+    loadComponent: () => import('./exos/components/exo1/exo1').then(m => m.Exo1),
+  },
+  {
+    path: 'exo2',
+    loadComponent: () => import('./exos/components/exo2/exo2').then(m => m.Exo2),
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { Demo1 } from "./demos/components/demo1/demo1";
-import { Demo2 } from "./demos/components/demo2/demo2";
-import { Exo1 } from './exos/components/exo1/exo1';
-import { Demo3 } from "./demos/components/demo3/demo3";
-import {Exo2} from './exos/components/exo2/exo2';
+import {RouterOutlet} from '@angular/router';
+import {Nav} from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [Demo1, Demo2, Exo1, Demo3, Exo2],
+  imports: [
+    RouterOutlet,
+    Nav
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
