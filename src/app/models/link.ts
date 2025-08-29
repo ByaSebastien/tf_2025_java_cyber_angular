b5e3f2923e6dbd8b11be2432a8d5017ae35a7571
Bya Sebastien <byaebastien@hotmail.com>
2025-08-29 14:33:26 +0200

    Router

---
+++ src/app/models/link.ts
@@ -0,0 +1,5 @@
+// Création d'un type de donnée pour typer mes lien de la nav
+export interface Link {
+  title: string;
+  url: string;
+}
---
+++ src/app/nav/nav.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {Link} from '../models/link';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
+  templateUrl: './nav.html',
+  styleUrl: './nav.scss'
+})
+export class Nav {
+
+  links: Link[] = [
+    { title: 'Bindings' , url: '/demo1'},
+    { title: 'Pipes' , url: '/demo2'},
+    { title: 'Directives' , url: '/demo3'},
+    { title: 'Timer' , url: '/exo1'},
+    { title: 'Courses' , url: '/exo2'},
+  ];
+}
